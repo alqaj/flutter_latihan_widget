05ec71dd3f8c746d0c8e8779a7c33e1ae7bb146c
Alliq Nur Imanin Aji <alliq@aiia.co.id>
2021-06-15 20:44:06 +0700

    add Calendar Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -23,8 +27,10 @@ class _MyAppState extends State<MyApp> {
           ),
           body: Center(
               child: Column(children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 3),
+            Container(
+                // Text Input Biasa dg Maxlength 25, Tanpa di perlihatkan counter
+                margin:
+                    EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 3),
                 child: TextField(
                   maxLength: 25,
                   decoration: InputDecoration(
@@ -33,17 +39,18 @@ class _MyAppState extends State<MyApp> {
                     counterText: "",
                   ),
                 )),
-                Container(
+            Container(
+                // Text Input Biasa dg Maxlength 5, dengan di perlihatkan counter
                 margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
                 child: TextField(
                   maxLength: 5,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Nama Panggilan',
-                    counterText: "",
                   ),
                 )),
             Container(
+                // Text Input tanpa maxlength
                 margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
                 child: TextField(
                   decoration: InputDecoration(
@@ -51,16 +58,45 @@ class _MyAppState extends State<MyApp> {
                     labelText: 'Alamat',
                   ),
                 )),
-                 Container(
+            Container(
+                // Text Input untuk Number Only
                 margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
                 child: TextField(
                   keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Usia',
                   ),
                 )),
+            Container(
+                // Text Input untuk Number Only dengan disertai Min dan Max Value dengan memanfaatkan CustomRangeFormater
+                margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    CustomRangeTextInputFormatter(),
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Jumlah Saudara (max 5)',
+                  ),
+                )),
+                Container(
+                  // Date Picker
+                  margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
+                  child: BasicDateField(
+                  )
+                ),
+                Container(
+                  // Time Picker
+                  margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
+                  child: BasicTimeField(
+                  )
+                ),
             Container(
               margin: EdgeInsets.only(left: 10, right: 10, top: 3, bottom: 3),
               width: double.infinity,
@@ -81,5 +117,69 @@ class _MyAppState extends State<MyApp> {
             )
           ]))),
     );
+  }
+}
+
+class CustomRangeTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    if (newValue.text == '')
+      return TextEditingValue();
+    else if (int.parse(newValue.text) < 0)
+      return TextEditingValue().copyWith(text: '0');
+
+    return int.parse(newValue.text) > 5
+        ? TextEditingValue().copyWith(text: '5')
+        : newValue;
+  }
+}
+
+class BasicDateField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      DateTimeField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Tanggal Lahir"
+        ),
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
+  }
+}
+
+class BasicTimeField extends StatelessWidget {
+  final format = DateFormat("HH:mm");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      // Text('Basic time field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+         decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Jam Lahir"
+        ),
+        onShowPicker: (context, currentValue) async {
+          final TimeOfDay? time = await showTimePicker(
+            context: context,
+            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+          );
+          return time == null ? null : DateTimeField.convert(time);
+        },
+      ),
+    ]);
   }
 }
